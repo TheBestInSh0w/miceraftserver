88289663cd2f3e49f6a4e6aa79c2845a75ce790f
TheBestInSh0w <kevinisthecoolest19@gmail.com>
2026-04-07 09:53:08 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Download server.jar from Google Drive
-curl -L -o server.jar "https://drive.google.com/uc?export=download&id=1_E6MtCnXL0PSdTIGZGDYCcvfG0lIrvmS"
-
-# Run the server
-java -Xms512M -Xmx512M -jar server.jar nogui
+java -Xms512M -Xmx1024M -jar EaglerXServer.jar
